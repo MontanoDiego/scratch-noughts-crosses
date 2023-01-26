e1c6bcd12ea709cf867b47f804578b4b2eea59a0
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-25 22:28:32 -0800

    hard coded and styled boxes for play

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,7 +7,21 @@ export default function Main() {
   // hardcode for styling purposes
   return (
     <div>
-      
+      <section>
+        <h3>x it is your turn!</h3>
+        <h2>Cats Game!</h2>
+      </section>
+      <section className='box-container'>
+        <div className='play-box'>Box1</div>
+        <div className='play-box'>Box2</div>
+        <div className='play-box'>Box3</div>
+        <div className='play-box'>Box4</div>
+        <div className='play-box'>Box5</div>
+        <div className='play-box'>Box6</div>
+        <div className='play-box'>Box7</div>
+        <div className='play-box'>Box8</div>
+        <div className='play-box'>Box9</div>
+      </section>
     </div>
   );
 }
